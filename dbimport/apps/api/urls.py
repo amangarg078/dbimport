d8bdafb2bcd565eb7a5f7fd9be94d4a13dcfdff3
Aman Garg <amangarg078@gmail.com>
2025-02-07 03:32:23 +0530

    fix urls

--- dbimport/apps/api/urls.py
+++ dbimport/apps/api/urls.py
@@ -3,9 +3,6 @@ from .views import LocationListCreateView, LocationRetrieveUpdateDestroyView
 
 app_name = 'api'
 
-router = DefaultRouter()
-router.register(r'locations', LocationViewSet)
-
 urlpatterns = [
     path('locations/', LocationListCreateView.as_view(), name='location-list-create'),
     path('locations/<str:unloc_code>/', LocationRetrieveUpdateDestroyView.as_view(), name='location-retrieve-update-destroy'),
